7ee2f9cee0c606e77e3933fdca10d3aebcd330ca
nazrul-avash <nazrulavash@gmail.com>
2019-09-19 18:27:24 +0600

    adds Advanced Encryption System implementation

---
+++ src/blockCipher/prac.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blockCipher;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nazrul
+ */
+public class prac {
+    public static void main(String[] args) {
+      String str ="abcdefabc";
+    str=String.format("32x", Integer.parseInt(str, 16));
+        System.out.println(str);
+    }
+    
+}
